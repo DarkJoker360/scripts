22377de0c8ae5ed13001a760eb770da4572ff896
DarkJoker360 <simoespo159@gmail.com>
2020-11-24 11:12:08 +0100

    scripts: update ginkgo-R.sh

--- ginkgo-R.sh
+++ ginkgo-R.sh
@@ -11,6 +11,11 @@ rm -rf hardware/qcom-caf/sm8150/display && git clone https://github.com/DarkJoke
 rm -rf hardware/qcom-caf/sm8150/media && git clone https://github.com/DarkJoker360/android_hardware_qcom_media -b $BRANCH hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/audio && git clone https://github.com/DarkJoker360/android_hardware_qcom_audio -b $BRANCH hardware/qcom-caf/sm8150/audio
 
+cd device/xiaomi/sm6125-common
+wget https://gist.githubusercontent.com/DarkJoker360/7424652e1205268d0c1b4afa48720202/raw/20c7a67087afb08aaf654a9748c596c72a8ace1a/0001-DNM-sm6125-common-Drop-in-tree-display-hal.patch
+git apply 0001-DNM-sm6125-common-Drop-in-tree-display-hal.patch
+cd ../../..
+
 . build/envsetup.sh
 
 # QTI BT stack
