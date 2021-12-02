03419de0ba1b573ccb8297c50465c10aac48df21
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-12-02 13:00:33 +0100

    arch: Add a script to install nvidia drivers and stuff

---
+++ arch/install_nvidia.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+read -e -p "Are you using any custom kernel? [Y/n]: " input
+if [[ "$input" =~ ^[Yy]$ ]]; then
+sudo pacman -Sy nvidia-dkms nvidia-utils nvidia-settings libvdpau-va-gl
+else
+sudo pacman -Sy nvidia nvidia-utils nvidia-settings libvdpau-va-gl
+fi
+
+#Stuff for people with a integrated + dedicated GPU
+read -e -p "Do you have Intel integrated GPU and discrete NVIDIA GPU? [y/n]: " input
+if [[ "$input" =~ ^[Yy]$ ]]; then
+yay -Sy system76-power
+
+# Stuff for system76-power to work properly
+sudo systemctl enable system76-power.service
+sudo systemctl start system76-power.service
+sudo system76-power graphics hybrid
+fi
