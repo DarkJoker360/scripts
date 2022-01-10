c4a7ee452feb69cb26e78dc5e8d9678ff44cbae5
DarkJoker360 <simoespo159@gmail.com>
2022-01-10 12:52:57 +0100

    custom_lineage_otas_info_gen: Print out json structure
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- android/custom_lineage_otas_info_gen.sh
+++ android/custom_lineage_otas_info_gen.sh
@@ -15,15 +15,23 @@ ID=$(md5sum lineage*-UNOFFICIAL-$DEVICE_CODENAME.zip)
 MD5=$(echo ${ID:0:32})
 SIZE=$(stat -c %s lineage*-UNOFFICIAL-$DEVICE_CODENAME.zip)
 
-printf "\n******** LineageOS ota info  ********\n\n"
-echo "datetime: $BUILD_DATE"
-echo "filename: $LINEAGE_FILE"
-echo "id: $MD5"
-echo "romtype: unofficial"
-echo "url: -"
-echo "version: $LINEAGE_VERSION"
-echo "size: $SIZE"
-printf "\n*******************************************************************"
+printf "\n******** LineageOS ota info  ********\n\n\n"
+
+echo '{
+  "response": [
+    {
+      "datetime": '$BUILD_DATE',
+      "filename": "'$LINEAGE_FILE'",
+      "id": "'$MD5'",
+      "romtype": "unofficial",
+      "size": '$SIZE',
+      "url": "-",
+      "version": "'$LINEAGE_VERSION'"
+    }
+  ]
+}'
+
+printf "\n\n*******************************************************************"
 echo "Update your json with the latest update info !"
 printf "For any question about the json file formatting see:\nhttps://github.com/LineageOS/android_packages_apps_Updater/blob/lineage-17.1/README.md\n"
 echo "*******************************************************************"
