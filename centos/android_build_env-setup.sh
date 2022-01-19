c3e1efa852470f0381ac137111858ff1b95a7080
DarkJoker360 <simoespo159@gmail.com>
2022-01-19 20:29:10 +0100

    centos: android_build_env-setup: Include End Point CentOS 7 repo
    * This will allow us to have latest packages version.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- centos/android_build_env-setup.sh
+++ centos/android_build_env-setup.sh
@@ -9,9 +9,10 @@
 # Android build env setup script for CentOS
 #
 
-sudo yum update
+sudo yum -y update
+sudo yum -y install https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.9-1.x86_64.rpm
 
-sudo yum install bison build-essential curl flex git gnupg gperf liblz4-tool libncurses5-dev wget \
+sudo yum -y install bison build-essential curl flex git gnupg gperf liblz4-tool libncurses5-dev wget \
     libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop maven pngcrush schedtool squashfs-tools \
     xsltproc zip repo openjdk-8-jdk bc ccache automake unzip zlib1g-dev g++-multilib python-networkx \
     libbz2-dev libbz2-1.0 libghc-bzlib-dev bzip2 dpkg-dev make optipng gcc-multilib lib32ncurses5-dev \
