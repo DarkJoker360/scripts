7344bc6a9aaf5d60d12b81d3ba9b69bc66873844
DarkJoker360 <simoespo159@gmail.com>
2022-01-21 10:51:14 +0100

    centos: android_build_env-setup: Install patch aswell
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- centos/android_build_env-setup.sh
+++ centos/android_build_env-setup.sh
@@ -16,6 +16,7 @@ sudo yum -y install bison build-essential curl flex git gnupg gperf liblz4-tool 
     libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop maven pngcrush schedtool squashfs-tools \
     xsltproc zip repo openjdk-8-jdk bc ccache automake unzip zlib1g-dev g++-multilib python-networkx \
     libbz2-dev libbz2-1.0 libghc-bzlib-dev bzip2 dpkg-dev make optipng gcc-multilib lib32ncurses5-dev \
-    lib32readline6-dev lib32z1-dev java-1.7.0-openjdk java-1.7.0-openjdk-devel glibc.i686 libstdc++.i686
+    lib32readline6-dev lib32z1-dev java-1.7.0-openjdk java-1.7.0-openjdk-devel glibc.i686 libstdc++.i686 \
+    patch
 
 wget https://raw.githubusercontent.com/DarkJoker360/scripts/master/common/repo.sh
